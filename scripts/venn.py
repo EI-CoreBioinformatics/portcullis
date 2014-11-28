23f2b8f616894cd24ffd60feee04e91a00ab1a3e
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2014-11-28 17:04:43 +0000

    Some tweaks to the python scripts.

--- scripts/venn.py
+++ scripts/venn.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from matplotlib_venn import venn3, venn3_circles, venn3_unweighted
 import sys
-def loadbed(filepath) :
+def loadbed(filepath, usestrand) :
     with open(filepath) as f:
         index = 0
         items = set()
@@ -15,7 +15,9 @@ def loadbed(filepath) :
                 start = words[6]
                 end = words[7]
                 strand = words[5]
-                key = chr + "_" + start + "_" + end + "_" + strand
+                key = chr + "_" + start + "_" + end
+                if usestrand:
+                    key += "_" + strand
                 items.add(key)
             index += 1
     if len(items) != index :
@@ -79,13 +81,13 @@ def filttab(tabfile, bedfile, filttabfile) :
 
 
 
-trueset = loadbed(sys.argv[1])
-alignedset = loadbed(sys.argv[2])
-filteredset = loadbed(sys.argv[3])
+trueset = loadbed(sys.argv[1], 0)
+alignedset = loadbed(sys.argv[2], 0)
+filteredset = loadbed(sys.argv[3], 0)
 
 plt.figure(figsize=(10,10))
-v = venn3([trueset, alignedset, filteredset], set_labels = (sys.argv[1], sys.argv[2], sys.argv[3]))
-#v = venn3_unweighted([trueset, alignedset, filteredset], set_labels = (sys.argv[1], sys.argv[2], sys.argv[3]))
+#v = venn3([trueset, alignedset, filteredset], set_labels = (sys.argv[1], sys.argv[2], sys.argv[3]))
+v = venn3_unweighted([trueset, alignedset, filteredset], set_labels = (sys.argv[1], sys.argv[2], sys.argv[3]))
 #c = venn3_circles([trueset, alignedset, filteredset], linestyle='dashed')
 #c[0].set_lw(1.0)
 #c[0].set_ls('dotted')
