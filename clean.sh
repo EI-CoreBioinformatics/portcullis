7bf488f86c2fbbbaecd75a5bbe9086a27af42bb0
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2015-04-15 12:39:14 +0100

    Adding htslib and samtools to codebase in order to reduce number of dependencies and guarentee we have a compatible version of samtools to work with.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-rm -r -f autom4te.cache build-aux autoscan.* Makefile Makefile.in aclocal.m4 configure config.* *.scan stamp-h1 */*.o *~ portculis-* portculis.pro.user src/Makefile src/Makefile.in tests/Makefile tests/Makefile.in clean
+rm -r -f autom4te.cache build-aux autoscan.* Makefile Makefile.in aclocal.m4 configure config.* *.scan stamp-h1 */*.o *~ portcullis-* portcullis.pro.user src/Makefile src/Makefile.in tests/Makefile tests/Makefile.in clean
 
 
