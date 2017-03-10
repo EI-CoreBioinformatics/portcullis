e2cf463893dadcdeba97a7c518fbf657f54639f6
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2017-03-10 17:30:13 +0000

    Added functionality to junctools to compare one or more GTF files based on a set of provided junctions.

--- scripts/junctools/gtf.py
+++ scripts/junctools/gtf.py
@@ -3,11 +3,13 @@ import collections
 from enum import Enum, unique
 
 from .junction import Junction, BedJunction
+from .performance import Performance
 
 @unique
 class Mode(Enum):
 	MARKUP = 1
 	FILTER = 2
+	COMPARE = 3
 
 def decstart(junctions):
 	last_id = ""
@@ -96,79 +98,123 @@ def loadgtf(filepath, use_strand=False):
 
 	return intron_chains, junc_key_set, nb_transcripts, nb_introns
 
+def run_compare(args, ref_juncs):
+	print("\t".join(["file", "j_distinct", "j_total", "j_tp", "j_fp", "j_fn", "j_recall", "j_precision", "j_f1",
+					 "t_transcripts", "t_monoexonic", "t_multiexonic", "t_supported", "t_unsupported", "t_precision"]))
+	for i in args.input:
+		intron_chains, junc_set, nb_transcripts, nb_introns = loadgtf(i, use_strand=not args.ignore_strand)
+		nb_monoexonic = nb_transcripts - len(intron_chains)
+		nb_multiexonic = len(intron_chains)
+
+		jr_tp = len(ref_juncs & junc_set)
+		jr_fn = len(ref_juncs - junc_set)
+		jr_fp = len(junc_set - ref_juncs)
+		jr_perf = Performance(tp=jr_tp, fn=jr_fn, fp=jr_fp)
+
+		nb_in_ref = 0
+		for t, introns in intron_chains.items():
+
+			is_valid_by_ref = True
+
+			for index, j in enumerate(introns):
+				in_ref = j.key in ref_juncs
+
+				if not in_ref:
+					is_valid_by_ref = False
+					break
+
+			if is_valid_by_ref:
+				nb_in_ref += 1
+
+		nb_unsupported = nb_multiexonic - nb_in_ref
+		t_precision = (nb_in_ref / nb_multiexonic) * 100.0
+
+		print("\t".join(str(_) for _ in [i, len(junc_set), nb_introns, jr_tp, jr_fp, jr_fn,
+										 "{0:.2f}".format(jr_perf.recall()), "{0:.2f}".format(jr_perf.precision()), "{0:.2f}".format(jr_perf.F1()),
+										 nb_transcripts, nb_monoexonic, nb_multiexonic, nb_in_ref, nb_unsupported, "{0:.2f}".format(t_precision)]))
+
+
 def gtf(args):
 	mode = Mode[args.mode.upper()]
-	print("Running junctools GTF in", mode.name, "mode")
+	print("# Running junctools GTF in", mode.name, "mode")
 
-	print("Loading junctions ...",end="")
+	if mode != Mode.COMPARE and (len(args.input) > 1 or len(args.input) == 0):
+		raise SyntaxError("This mode can takes a single GTF file as input")
+
+	print("# Loading junctions ...",end="")
 	port_juncs, port_count = Junction.createJuncSet(args.junctions, use_strand=not args.ignore_strand)
 	print(" done.  Found", port_count, "junctions.")
-	print("Loading transcripts ...",end="")
-	intron_chains, junc_set, nb_transcripts, nb_introns = loadgtf(args.input, use_strand=not args.ignore_strand)
-	print(" done.")
-	nb_monoexonic = nb_transcripts - len(intron_chains)
-	nb_multiexonic = len(intron_chains)
-	print("Found", nb_transcripts, "transcripts. ", nb_monoexonic, "are monoexonic and", nb_multiexonic, "are multiexonic.")
-	print("Found", len(junc_set), "distinct junctions and", nb_introns, "total junctions in GTF.")
-	print()
 
-	print("Doing junction level comparison ...", end="")
-	juncs_inport = port_juncs & junc_set
-	print(" done.")
-	recall = (len(juncs_inport) / port_count) * 100.0
-	precision = (len(juncs_inport) / len(junc_set)) * 100.0
-	print(len(juncs_inport), "/", port_count, "(" + "{0:.2f}".format(recall) + ") of valid junctions.")
-	print(len(juncs_inport), "/", len(junc_set), "(" + "{0:.2f}".format(precision) + ") junctions supported by input.")
-	print()
+	if mode == Mode.COMPARE:
+		run_compare(args, port_juncs)
 
+	else:
+		print("Loading transcripts ...",end="")
+		intron_chains, junc_set, nb_transcripts, nb_introns = loadgtf(args.input[0], use_strand=not args.ignore_strand)
+		print(" done.")
+		nb_monoexonic = nb_transcripts - len(intron_chains)
+		nb_multiexonic = len(intron_chains)
+		print("Found", nb_transcripts, "transcripts. ", nb_monoexonic, "are monoexonic and", nb_multiexonic, "are multiexonic.")
+		print("Found", len(junc_set), "distinct junctions and", nb_introns, "total junctions in GTF.")
+		print()
 
-	print("Identifying invalid multi-exonic transcripts ... ", end="")
-	invalid_transcripts = collections.defaultdict(list)
-	for t, introns in intron_chains.items():
-		for index, j in enumerate(introns):
-			if not j.key in port_juncs:
-				invalid_transcripts[t].append(str(j.start+1) + "_" + str(j.end+1))
-	print(" done.  Found", len(invalid_transcripts), "/", nb_multiexonic, "invalid multi-exonic transcripts.")
-	print()
+		print("Doing junction level comparison ...", end="")
+		juncs_inport = port_juncs & junc_set
+		print(" done.")
+		recall = (len(juncs_inport) / port_count) * 100.0
+		precision = (len(juncs_inport) / len(junc_set)) * 100.0
+		print(len(juncs_inport), "/", port_count, "(" + "{0:.2f}".format(recall) + ") of valid junctions.")
+		print(len(juncs_inport), "/", len(junc_set), "(" + "{0:.2f}".format(precision) + ") junctions supported by input.")
+		print()
 
 
-	print("Writing output to", args.output," ... ", end="")
-	o = open(args.output, mode='w')
+		print("Identifying invalid multi-exonic transcripts ... ", end="")
+		invalid_transcripts = collections.defaultdict(list)
+		for t, introns in intron_chains.items():
+			for index, j in enumerate(introns):
+				if not j.key in port_juncs:
+					invalid_transcripts[t].append(str(j.start+1) + "_" + str(j.end+1))
+		print(" done.  Found", len(invalid_transcripts), "/", nb_multiexonic, "invalid multi-exonic transcripts.")
+		print()
 
-	# Create dict of transcripts with all exons in memory
-	with open(args.input) as f:
-		for l in f:
-			line = l.strip()
-			if not line.startswith("#"):
-				parts = line.split('\t')
-				if len(parts) == 9 and (parts[2] == "exon" or parts[2] == "transcript"):
-					tags = parts[8].split(';')
-					for tag in tags:
-						t = tag.strip()
-						if t:
-							tag_parts = t.split()
-							if tag_parts[0] == "transcript_id":
-								tid = tag_parts[1].strip()
-								transcript_id = tid[1:-1] if tid[0] == '\"' else tid
-								if transcript_id in invalid_transcripts:
-									if mode != Mode.FILTER:
+
+		print("Writing output to", args.output," ... ", end="")
+		o = open(args.output, mode='w')
+
+		# Create dict of transcripts with all exons in memory
+		with open(args.input[0]) as f:
+			for l in f:
+				line = l.strip()
+				if not line.startswith("#"):
+					parts = line.split('\t')
+					if len(parts) == 9 and (parts[2] == "exon" or parts[2] == "transcript"):
+						tags = parts[8].split(';')
+						for tag in tags:
+							t = tag.strip()
+							if t:
+								tag_parts = t.split()
+								if tag_parts[0] == "transcript_id":
+									tid = tag_parts[1].strip()
+									transcript_id = tid[1:-1] if tid[0] == '\"' else tid
+									if transcript_id in invalid_transcripts:
+										if mode != Mode.FILTER:
+											if mode == Mode.MARKUP and parts[2] == "transcript":
+												bad_juncs = ",".join(invalid_transcripts[transcript_id])
+												o.write(line + " introns \"invalid(" + bad_juncs + ")\";\n")
+											else:
+												o.write(line + "\n")
+									else:
 										if mode == Mode.MARKUP and parts[2] == "transcript":
-											bad_juncs = ",".join(invalid_transcripts[transcript_id])
-											o.write(line + " introns \"invalid(" + bad_juncs + ")\";\n")
+											o.write(line + " introns \"valid\";\n")
 										else:
 											o.write(line + "\n")
-								else:
-									if mode == Mode.MARKUP and parts[2] == "transcript":
-										o.write(line + " introns \"valid\";\n")
-									else:
-										o.write(line + "\n")
+					else:
+						o.write(line + "\n")
 				else:
 					o.write(line + "\n")
-			else:
-				o.write(line + "\n")
 
-	o.close()
-	print(" done.")
+		o.close()
+		print(" done.")
 
 
 def add_options(parser):
@@ -179,12 +225,13 @@ def add_options(parser):
 
 	parser.add_argument("-j", "--junctions", required=True, help='''The file containing junctions that should be found in the GTF.''')
 
-	parser.add_argument("-o", "--output", required=True, default="junctools.out.gtf",
+	parser.add_argument("-o", "--output", default="junctools.out.gtf",
 						help="The filtered or markedup GTF output.  By default we print to stdout.")
 
 	parser.add_argument("mode", help='''GTF operation to apply.  See above for details.  Available options:
  - filter
  - markup
+ - compare
 ''')
 
-	parser.add_argument("input", help="The input GTF file to convert")
+	parser.add_argument("input", nargs="+", help="The input GTF file to convert")
